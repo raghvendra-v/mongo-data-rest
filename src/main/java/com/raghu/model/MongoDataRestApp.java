a567328b321a74a9c52ac6c3025fe009b11fd98c
raghvendra-v <raghvendrav@gmail.com>
2020-01-10 02:15:02 +0530

    test 2

--- src/main/java/com/raghu/model/MongoDataRestApp.java
+++ src/main/java/com/raghu/model/MongoDataRestApp.java
@@ -3,10 +3,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingMongodbDataRestApplication {
+public class MongoDataRestApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccessingMongodbDataRestApplication.class, args);
+		SpringApplication.run(MongoDataRestApp.class, args);
 	}
 
 }
--- src/test/java/com/raghu/model/MongoDbSpringIntegrationTest.java
+++ src/test/java/com/raghu/model/MongoDbSpringIntegrationTest.java
@@ -1,54 +1,40 @@
 package com.raghu.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.DBObject;
+/*
+ * 
+ * //TestRestTemplate testRestTemplate = new TestRestTemplate(restTemplateBuilder, username, password, httpClientOptions)
+		//String responseFromTestRestTemplate = testRestTemplate.postForObject("/people", p, String.class);
+		//assertNotNull(responseFromTestRestTemplate);
 
+		ResultActions resultActions = mockMvc.perform(
+				post("/people").content(objectMapper.writeValueAsString(p)).contentType(MediaType.APPLICATION_JSON));
+		resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+		resultActions.andDo(mvcResult -> {
+			String responseFromMockMvc = mvcResult.getResponse().getContentAsString();
+		});
+ */
 @DataMongoTest
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
 public class MongoDbSpringIntegrationTest {
 	@Autowired
-	private MockMvc mockMvc;
+	private MongoTemplate mongoTemplate;
+
 	@Autowired
-	private ObjectMapper objectMapper;
-
-	@DisplayName("given object to save" + " when save object using MongoDB template" + " then object is saved")
-	@Test
-	@Disabled
-	public void test(@Autowired MongoTemplate mongoTemplate) {
-		// given
-		DBObject objectToSave = BasicDBObjectBuilder.start().add("key", "value").get();
-
-		// when
-		mongoTemplate.save(objectToSave, "collection");
-
-		// then
-		assertThat(mongoTemplate.findAll(DBObject.class, "collection")).extracting("key").containsOnly("value");
-	}
+	private PersonRepository personRepository;
 
 	@Test
 	@DisplayName("Save Marilyn Monroe")
@@ -61,16 +47,12 @@ public class MongoDbSpringIntegrationTest {
 		p.setPlaceOfBirth("Los Angeles, California, U.S.");
 		byte[] bytes = Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("Monroecirca1953.jpg").toURI()));
 		p.setProfileImage(new Binary(BsonBinarySubType.USER_DEFINED, bytes));
-		//TestRestTemplate testRestTemplate = new TestRestTemplate(restTemplateBuilder, username, password, httpClientOptions)
-		//String responseFromTestRestTemplate = testRestTemplate.postForObject("/people", p, String.class);
-		//assertNotNull(responseFromTestRestTemplate);
+		mongoTemplate.save(p);
 
-		ResultActions resultActions = mockMvc.perform(
-				post("/people").content(objectMapper.writeValueAsString(p)).contentType(MediaType.APPLICATION_JSON));
-		resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-		resultActions.andDo(mvcResult -> {
-			String responseFromMockMvc = mvcResult.getResponse().getContentAsString();
-		});
+		List<Person> found = personRepository.findByName("Marilyn Monroe");
+
+		// then
+		assertThat(found.get(0).getName()).isEqualTo("Marilyn Monroe");
 
 	}
 }
